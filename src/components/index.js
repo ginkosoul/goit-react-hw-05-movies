5fd6ca5824041132a52dc2b5ba4856bc97b46c7a
ginkosoul <ginkosoul@gmail.com>
2023-06-03 17:34:09 +0300

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export * from 'components/Layout';
+export { default as Cast} from 'components/Cast';
+export { default as Reviews} from 'components/Reviews';
---
+++ src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as Home } from 'pages/Home';
+export { default as Movies } from 'pages/Movies';
+export { default as MovieDetails } from 'pages/MovieDetails';
